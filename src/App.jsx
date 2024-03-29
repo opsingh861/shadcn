2ebeccb1f2a4d98dd1333198970c740fabdb6380
Aditya Dhanraj <opsingh861@gmail.com>
2024-03-30 00:30:43 +0530

    button created

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+
+  return (
+    <>
+      <Button>Click me</Button>
+      <h1 className="underline">App</h1>
+    </>
+  )
+}
+
+export default App
